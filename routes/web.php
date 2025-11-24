685ec5cd90679fb139e8fa0cfcd2aefc485fc5c0
Ghaffari <mmgh97959@gmail.com>
2025-11-24 18:29:19 +0330

    Add APIs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\API\ArtistController;
+use App\Http\Controllers\API\PlaylistController;
+use App\Http\Controllers\API\SongController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('api')->middleware('api')->group(function () {
+    Route::apiResource('artists', ArtistController::class);
+    Route::apiResource('songs', SongController::class);
+    Route::apiResource('playlists', PlaylistController::class);
 });
